3c068f13b59816b53459f9646b4890265b7ace5d
JASPRICO <albe-025@hotmail.com>
2025-02-28 17:02:29 -0500

    Add files via upload
    FINAL

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ const mysql = require('mysql2/promise');
 
 // Create the connection pool. The pool-specific settings are the defaults
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'xexpress',
+  host: 'mysql-29bf9b16-albe-d062.d.aivencloud.com',
+  user: 'avnadmin',
+  database: 'defaultdb',
+  password: 'defaultdb',
+  port: 25289,
 });
 
 // Middleware to parse JSON request bodies
